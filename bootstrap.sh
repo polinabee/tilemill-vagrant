11005da86a521db7f5f559ab6158df7f950e4dbd
Polina Berenstein <polinabee@users.noreply.github.com>
2016-12-06 12:01:23 -0800

    updating config file statement, unpacked install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
-apt-get update
-wget http://tilemill.s3.amazonaws.com/latest/install-tilemill.tar.gz
+sudo apt-get update
+wget --quiet http://tilemill.s3.amazonaws.com/latest/install-tilemill.tar.gz
 tar -xf install-tilemill.tar.gz
-sudo ./install-tilemill.sh
+
+#LINECOUNT=`wc -l install-tilemill.sh | awk '{print $1}'`
+#LINECOUNT=$(( $LINECOUNT-1 ))
+
+sudo pkexec /bin/bash -c "apt-get install --yes python-software-properties &&
+    yes | apt-add-repository ppa:developmentseed/mapbox &&
+    apt-get update --yes &&
+    apt-get install --yes tilemill"
+
+echo "{
+    "'"port"'": 20009,
+    "'"tilePort"'": 20008,
+    "'"files"'": "'"~/Documents/MapBox"'",
+    "'"examples"'": true,
+    "'"sampledata"'": true,
+    "'"host"'": false,
+    "'"listenHost"'": "'"0.0.0.0"'",
+    "'"syncURL"'": "'"https://api.mapbox.com"'",
+    "'"server"'": false,
+    "'"verbose"'": "'"on"'"
+}" | sudo tee '/usr/share/tilemill/lib/config.defaults.json'
